08bd4defc4d30c9a8ac3a30ed66d8516e6253e48
tegami2007@mail.ru <tegami2007@mail.ru>
2019-05-17 12:45:42 +0300

    Добавьте файлы проекта.

---
+++ B3-P9_9_CreaditCalculator/CreaditCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3_P9_9_CreaditCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму кредита:");
+            string creditstr = Console.ReadLine();
+            int credit = Convert.ToInt32(creditstr);
+
+            Console.WriteLine("Введите процент кредита:");
+            string percentstr = Console.ReadLine();
+            decimal percent = Convert.ToDecimal(percentstr);
+
+            decimal payment;
+            decimal balance = credit;
+            decimal s =0;
+
+            Console.WriteLine("Выплаты по месяцам:");
+            for (int i = 0; i < 12; i++)
+            {
+                payment = credit / 12 + balance * percent / 100 / 12;
+                Console.WriteLine($"{i+1} месяц: {Math.Round(payment, 2)} руб.");
+                balance = balance - (decimal)credit / 12;
+                //Console.WriteLine($"{Math.Round(balance, 2)}");
+                s = s + payment;
+            }
+            Console.WriteLine($"Общая сумма выплат составит: {Math.Round(s, 2)} руб.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ B3-P9_9_CreaditCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("B3-P9_9_CreaditCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("B3-P9_9_CreaditCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b59af063-5af4-4360-8b5c-770d2c6088c1")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
